eedf5451ead70ae3a0e8aed18d569c77d697190d
bjogden <bjogden123@gmail.com>
2022-08-17 18:04:15 -0500

    add users app

--- app/settings.py
+++ app/settings.py
@@ -30,15 +30,21 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+THIRD_PARTY_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # 'rest_framework',
 ]
+
+LOCAL_APPS = [
+    "app.apps.users",
+]
+
+INSTALLED_APPS = THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
