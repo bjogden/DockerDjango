4e61f3f9fd357735f87fec9693c1fa71401ee75a
bjogden <bjogden123@gmail.com>
2022-08-17 20:26:59 -0500

    add DRF pagination class default

--- app/settings.py
+++ app/settings.py
@@ -120,6 +120,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Django Rest Framework
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
