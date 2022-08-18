eefd9235344d8425f03a0035cb69aba5f6d1427e
bjogden <bjogden123@gmail.com>
2022-08-18 10:34:37 -0500

    add user test

--- app/apps/users/tests.py
+++ app/apps/users/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import User
+
+
+class UserViewTestCase(TestCase):
+    API = "/api/users/"
+
+    def setUp(self) -> None:
+        self.request = APIClient()
+
+        User.objects.bulk_create(
+            [
+                User(email="foo@bar.com"),
+                User(email="yee@haw.com"),
+            ]
+        )
+
+    def test_get_users(self):
+        response = self.request.get(self.API, format="json")
+        self.assertEqual(200, response.status_code)
+        output = response.data
+        self.assertEqual(2, output["count"])
+        self.assertEqual(2, len(output["results"]))
